b11b658bb62ff14231cd634eadbab4ac20b58b2d
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2020-11-17 11:31:31 +0700

    ASP .Net Day 1 - Introduction MVC & Models

---
+++ TrainingMSHR/Migrations/202011170328413_AddingModelDepartmentAndEmployee.cs
@@ -0,0 +1,43 @@
+﻿namespace TrainingMSHR.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingModelDepartmentAndEmployee : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        BirthDate = c.DateTimeOffset(nullable: false, precision: 7),
+                        Department_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Departments", t => t.Department_Id, cascadeDelete: true)
+                .Index(t => t.Department_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Employees", "Department_Id", "dbo.Departments");
+            DropIndex("dbo.Employees", new[] { "Department_Id" });
+            DropTable("dbo.Employees");
+            DropTable("dbo.Departments");
+        }
+    }
+}
---
+++ TrainingMSHR/Migrations/202011170341187_UpdateModelDepartment.cs
@@ -0,0 +1,18 @@
+﻿namespace TrainingMSHR.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateModelDepartment : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Departments", "CreatedOn", c => c.DateTimeOffset(nullable: false, precision: 7));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Departments", "CreatedOn");
+        }
+    }
+}
